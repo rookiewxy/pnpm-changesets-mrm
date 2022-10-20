a58d196022f6cf3320b8180e563a10296c6b6a47
wxy <2586958565@qq.com>
2022-10-20 08:49:25 +0800

    lerna版本升级v6

--- packages/react-demo/src/index.js
+++ packages/react-demo/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import a from 'b';
 import reportWebVitals from './reportWebVitals';
-
+console.log(a);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
